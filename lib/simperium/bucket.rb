aae0facbcb5108c4768f9c46e359b92db66f2866
James Kong <jkongie@gmail.com>
2014-01-16 16:19:48 +1100

    Rubyify the Bucket styles.

--- lib/simperium/bucket.rb
+++ lib/simperium/bucket.rb
@@ -26,35 +26,17 @@ module Simperium
       @clientid   = options[:clientid] || "rb-#{generate_ccid}"
     end
 
-    def _auth_header
-      headers = {"X-Simperium-Token" => "#{@auth_token}"}
-      headers["X-Simperium-User"] = @userid unless @userid.nil?
-      headers
-    end
-
-    def _request(url, data=nil, headers=nil, method=nil, timeout=nil)
-      url = "#{@scheme}://#{@host}/1/#{url}"
-      opts = {:url => url,
-              :method => :post,
+    def _request(url, data = nil, headers = {}, method = nil, timeout = nil)
+      url  = "#{@scheme}://#{@host}/1/#{url}"
+      opts = {:url          => url,
+              :method       => :post,
               :open_timeout => 30,
-              :timeout => 80}
+              :timeout      => 80 }
 
-      if data
-        opts = opts.merge({:payload => data})
-      end
-
-      if headers.nil?
-        headers = {}
-      end
-      opts = opts.merge({:headers => headers})
-
-      if method
-        opts = opts.merge({:method => method})
-      end
-
-      if timeout
-        opts = opts.merge({:timeout => timeout})
-      end
+      opts.merge!(:payload => data) if data
+      opts.merge!(:headers => headers)
+      opts.merge!(:method => method) if method
+      opts.merge!(:timeout => timeout)
 
       begin
         response = RestClient::Request.execute(opts)
@@ -77,21 +59,21 @@ module Simperium
         ErrorHandling.handle_restclient_error(e)
       end
 
-      return response
+      response
     end
 
-    def index(options={})
-      defaults = {:data=>nil, :mark=>nil, :limit=>nil, :since=>nil}
-      unless options.empty?
-        options = defaults.merge(options)
-      else
-        options = defaults
-      end
+    def index(opts = {})
+      opts = {
+        :data  => nil,
+        :mark  => nil,
+        :limit => nil,
+        :since => nil
+      }.merge(opts)
 
-      data = options[:data]
-      mark = options[:mark]
-      limit = options[:limit]
-      since = options[:since]
+      data  = opts[:data]
+      mark  = opts[:mark]
+      limit = opts[:limit]
+      since = opts[:since]
 
       url = "#{@appname}/#{@bucket}/index?"
       url += "&data=1" if data
@@ -99,53 +81,42 @@ module Simperium
       url += "&limit=#{limit.to_s}" if limit
       url += "&since=#{since.to_str}" if since
 
-      response = self._request(url, data=nil, headers=_auth_header(), method='GET')
-      return JSON.load(response.body)
+      response = self._request(url, nil, auth_header, 'GET')
+      JSON.load(response.body)
     end
 
-    def get(item, options={})
-      defaults = {:default=>nil, :version=>nil}
-      unless options.empty?
-        options = defaults.merge(options)
-      else
-        options = defaults
-      end
-      default = options[:default]
-      version = options[:version]
+    def get(item, opts = {})
+      opts = {
+        :default => nil,
+        :version => nil
+      }.merge(opts)
 
       url = "#{@appname}/#{@bucket}/i/#{item}"
-      url += "/v/#{version}" unless version.nil?
+      url += "/v/#{options[:version]}" unless opts[:version].nil?
 
-      response = self._request(url, data=nil, headers=_auth_header(), method='GET')
-      return JSON.load(response.body)
+      response = self._request(url, nil, auth_header, 'GET')
+      JSON.load(response.body)
     end
 
-    def post(item, data, options={})
-      defaults = {:version=>nil, :ccid=>nil, :include_response=>false, :replace=>false}
-      unless options.empty?
-        options = defaults.merge(options)
-      else
-        options = defaults
-      end
-
-      version = options[:version]
-      ccid = options[:ccid]
-      include_response = options[:include_response]
-      replace = options[:replace]
-
-      ccid = generate_ccid if ccid.nil?
+    def post(item, data, opts = {})
+      opts = {
+        :version          => nil,
+        :ccid             => nil,
+        :include_response => false,
+        :replace          => false
+      }.merge(opts)
 
       url = "#{@appname}/#{@bucket}/i/#{item}"
-      url += "/v/#{version}" if version
-      url += "?clientid=#{@clientid}&ccid=#{ccid}"
-      url += "&response=1" if include_response
-      url += "&replace=1" if replace
+      url += "/v/#{opts[:version]}" if opts[:version]
+      url += "?clientid=#{@clientid}&ccid=#{opts[:ccid] || generate_ccid}"
+      url += "&response=1" if opts[:include_response]
+      url += "&replace=1" if opts[:replace]
 
       data = JSON.dump(data)
 
-      response = self._request(url, data, headers=_auth_header())
+      response = self._request(url, data, auth_header)
 
-      if include_response
+      if opts[:include_response]
         return item, JSON.load(response.body)
       else
         return item
@@ -153,70 +124,53 @@ module Simperium
     end
 
     def new(data, ccid=nil)
-      self.post(generate_ccid, data, :ccid => ccid)
+      post generate_ccid, data, :ccid => ccid
     end
 
     def set(item, data, options={})
-      self.post(item, data, options)
+      post item, data, options
     end
 
     def delete(item, version=nil)
-      ccid = self.generate_ccid
       url = "#{@appname}/#{@bucket}/i/#{item}"
       url += "/v/#{version}" if version
-      url += "?clientid=#{@clientid}&ccid=#{ccid}"
+      url += "?clientid=#{@clientid}&ccid=#{generate_ccid}"
 
-      response = self._request(url, data=nil, headers=_auth_header(), method='DELETE')
+      response = self._request(url, nil, auth_header, 'DELETE')
 
-      if response.body.strip.nil?
-        return ccid
-      end
+      return ccid if response.body.strip.nil?
     end
 
-    def changes(options={})
-      defaults = {:cv=>nil, :timeout=>nil}
-      unless options.empty?
-        options = defaults.merge(options)
-      else
-        options = defaults
-      end
-
-      cv = option[:cv]
-      timeout = option[:timeout]
+    def changes(opts = {})
+      opts = {
+        :cv      => nil,
+        :timeout => nil
+      }.merge(opts)
 
       url = "#{@appname}/#{@bucket}/changes?clientid=#{@clientid}"
-      url += "&cv=#{cv}" unless cv.nil?
+      url += "&cv=#{opts[:cv]}" unless opts[:cv].nil?
 
-      headers = _auth_header()
-
-      response = self._request(url, data=nil, headers=headers, method='GET', timeout=timeout)
-      return JSON.load(response.body)
+      response = self._request(url, nil, auth_header, 'GET', opts[:timeout])
+      JSON.load(response.body)
     end
 
-    def all(options={})
-      defaults = {:cv=>nil, :data=>nil, :username=>false, :most_recent=>false, :timeout=>nil}
-      unless options.empty?
-        options = defaults.merge(options)
-      else
-        options = defaults
-      end
-
-      cv = options[:cv]
-      data = options[:data]
-      username = options[:username]
-      most_recent = options[:most_recent]
-      timeout = options[:timeout]
+    def all(opts = {})
+      opts = {
+        :cv          => nil,
+        :data        => nil,
+        :username    => false,
+        :most_recent => false,
+        :timeout     => nil
+      }.merge(opts)
 
       url = "#{@appname}/#{@bucket}/all?clientid=#{@clientid}"
-      url += "&cv=#{cv}" unless cv.nil?
-      url += "&username=1" if username
-      url += "&data=1" if data
-      url += "&most_recent=1" if most_recent
+      url += "&cv=#{opts[:cv]}" unless opts[:cv].nil?
+      url += "&username=1" if opts[:username]
+      url += "&data=1" if opts[:data]
+      url += "&most_recent=1" if opts[:most_recent]
 
-      headers = _auth_header()
-
-      response = self._request(url, data=nil, headers=headers, method='GET', timeout=timeout)
-      return JSON.load(response.body)
+      response = self._request(url, nil, auth_header, 'GET', opts[:timeout])
+      JSON.load(response.body)
     end
 
     private
@@ -226,6 +180,15 @@ module Simperium
     # Returns a String.
     def generate_ccid
       SecureRandom.uuid.delete('-')
+    end
+
+    # Returns the headers for a bucket request.
+    #
+    # Returns a Hash.
+    def auth_header
+      headers = {"X-Simperium-Token" => "#{@auth_token}"}
+      headers["X-Simperium-User"] = @userid unless @userid.nil?
+      headers
     end
   end
 end
